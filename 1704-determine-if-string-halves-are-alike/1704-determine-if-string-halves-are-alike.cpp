11dc88c3795be1c1d89b6961bf630e5dbb7c1782
jejuvita01 <63396820+jejuvita01@users.noreply.github.com>
2022-12-01 13:59:10 -0800

    Time: 4 ms (72.62%), Space: 6.6 MB (83.20%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        string a = s.substr(0, s.length() / 2);
-        string b = s.substr(s.length() / 2, s.length() / 2);
         int num = 0;
         for (int i = 0; i < s.length() / 2; i++)
-            if (tolower(a[i]) == 'a' || tolower(a[i]) == 'e' || tolower(a[i]) == 'i' || tolower(a[i]) == 'o' || tolower(a[i]) == 'u')
+            if (tolower(s[i]) == 'a' || tolower(s[i]) == 'e' || tolower(s[i]) == 'i' || tolower(s[i]) == 'o' || tolower(s[i]) == 'u')
                 num++;
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (tolower(b[i]) == 'a' || tolower(b[i]) == 'e' || tolower(b[i]) == 'i' || tolower(b[i]) == 'o' || tolower(b[i]) == 'u') {
+        for (int i = s.length() / 2; i < s.length(); i++) {
+            if (tolower(s[i]) == 'a' || tolower(s[i]) == 'e' || tolower(s[i]) == 'i' || tolower(s[i]) == 'o' || tolower(s[i]) == 'u')
                 num--;
-                if (num < 0)
-                    return false;
-            }
+            if (num < 0)
+                return false;
         }
         return (num == 0);
     }
