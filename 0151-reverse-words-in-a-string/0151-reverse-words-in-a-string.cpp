ad41d2303e37736f26022da849a345314112f510
Seohyun <63396820+jejuvita01@users.noreply.github.com>
2023-07-25 15:19:29 -0700

    Time: 4 ms (80.25%), Space: 7.4 MB (74.79%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string answer;
+        string tmp = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == ' ') {
+                if (tmp != "") {
+                    reverse(tmp.begin(), tmp.end());
+                    answer += tmp;
+                    while (s[i] == ' ' && i > 0)
+                        i--;
+                    if (i != 0 || s[i] != ' ') {
+                        answer += ' ';
+                    }
+                    i++;
+                    tmp = "";
+                }
+            }
+            else {
+                tmp += s[i];
+            }
+        }
+        if (tmp != "") {
+            reverse(tmp.begin(), tmp.end());
+            answer += tmp;
+        }
+
+        return answer;
+    }
+};
