413291fd514ec1b99174385f83946b6c8070875c
Seohyun <63396820+jejuvita01@users.noreply.github.com>
2023-07-26 14:37:37 -0700

    Time: 12 ms (58.52%), Space: 7.5 MB (12.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,7 +6,15 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
-        answer = (s.compare(t) == 0);
+        if (s.length() != t.length())
+            answer = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != t[i]) {
+                answer = false;
+                break;
+            }
+        }
         
         return answer;
     }
