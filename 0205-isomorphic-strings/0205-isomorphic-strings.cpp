306abeaaaf1e640753ce145832931d624eeda1b3
Seohyun <63396820+jejuvita01@users.noreply.github.com>
2023-08-06 15:58:45 -0700

    Time: 4 ms (82.62%), Space: 7.1 MB (11.94%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, char> pair;
+        vector<char> exist;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (pair.find(s[i]) == pair.end() && find(exist.begin(), exist.end(), t[i]) == exist.end()) {
+                pair.insert(make_pair(s[i], t[i]));
+                exist.push_back(t[i]);
+            }
+            else {
+                if (pair[s[i]] != t[i])
+                    return false;
+                if (pair.find(s[i]) == pair.end() && find(exist.begin(), exist.end(), t[i]) != exist.end())
+                    return false;
+            }
+        }
+        return true;
+    }
+};
