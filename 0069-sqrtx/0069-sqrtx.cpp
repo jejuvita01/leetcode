b35147dd625f0febcc623a58083374197fd414d4
Seohyun <63396820+jejuvita01@users.noreply.github.com>
2026-03-20 20:20:43 +0900

    Time: 31 ms (5.23%), Space: 8.5 MB (50%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return (int)sqrt(x);
+        for (long long i = 0; i <= x; i++) {
+            if (i * i == x)
+                return i;
+            else if (i * i > x)
+                return i - 1;
+        }
+        return 1;
     }
 };
